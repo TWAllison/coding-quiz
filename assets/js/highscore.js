940b43ea4d08e61ee614eb0041aea31fe9cb75b8
TWAllison <tylerallison55@gmail.com>
2021-08-31 07:09:26 -0600

    js for highscore page  store hs to local storage , reset highscore function

---
+++ assets/js/highscore.js
@@ -0,0 +1,27 @@
+var showHighscore = function () {
+
+    var highscore = JSON.parse(window.localStorage.getItem("highscore")) || [];
+
+    highscore.sort(function (a, b) {
+        return b.score - a.score;
+    });
+
+    highscore.array.forEach(function (score) {
+        var scoreLi = document.createElement("li");
+        scoreLi.textContent = score.initials + " - " + score.score;
+
+        var olEl = document.querySelector('#highscore');
+        olEl.appendChild(scoreLi);
+
+
+    });
+}
+
+function clearHighscore() {
+    window.localStorage.removeItem("highscore");
+    window.location.reload();
+}
+
+document.getElementById("clear").onclick = clearHighscore;
+
+showHighscore();
